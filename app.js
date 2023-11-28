db50ad8fdcebfe3f9b052e10dfaf8e0063853187
KirubG <141554091+KirubG@users.noreply.github.com>
2023-11-28 13:17:16 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let myAge = 25
+let yourAge = 35
+
+console.log("my age is: " + myAge)
+console.log("your age is: " + yourAge)
+
---
+++ tutorial.js
@@ -0,0 +1,18 @@
+// qestion 6
+let name, age, salary, position
+
+//question 7
+let price= 45
+let ishappy= true
+const pie =3.4
+let age_ = 14
+// question 8
+let first_name
+let last_name
+let issingle
+let country
+
+//question 9
+let firstname, lastname, is_single, nationality
+
+//question 10
